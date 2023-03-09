18654198f3571d834ee1576aaf53b66fd5d20c81
Will DeKeizer <127137974+willdekeizer@users.noreply.github.com>
2023-03-09 16:12:35 -0600

    Add files via upload

---
+++ cardgame.py
@@ -0,0 +1,34 @@
+import random
+score = 0
+
+while True:
+	score = score6
+	print("\nIs this card High or Low\n")
+	if number >= 8:
+		x = input()
+		if x == "High":
+			print("\nCorrect")
+			score = score + 1
+			
+		else:
+			print("\nWrong")
+			score = score - 1
+			
+	if number < 7:
+		x = input()
+		if x == "low":
+			print("\nCorrect")
+			score = score + 1
+		
+		else:
+			print("\nWrong")
+			score = score - 1
+		
+	print(str(score))
+	
+	
+			
+			
+
+	
+	
